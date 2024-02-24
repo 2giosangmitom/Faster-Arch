606af2335c9dfb63091fb0059d2f2c56fded91da
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-24 22:46:50 +0700

    update script

--- main.sh
+++ main.sh
@@ -1,67 +1,31 @@
 #!/usr/bin/bash
 
-NOCOLOR="\033[0m"
-RED="\033[31m"
-YELLOW="\033[33m"
-BLUE="\033[34m"
-PURPLE="\033[35m"
-CYAN="\033[36m"
-WHITE="\033[97m"
+# Update package repositories
+sudo pacman -Syu
 
-optimize() {
-	echo -e "$WHITE>> Update system$NOCOLOR"
-	sudo pacman -Syu
+# Install necessary tools for performance optimization
+sudo pacman -S --noconfirm --needed sysstat thermald
 
-	echo -e "$CYAN>> Install intel driver$NOCOLOR"
-	sudo pacman -S intel-media-driver --noconfirm --needed
+# Enable services
+sudo systemctl enable --now sysstat
+sudo systemctl enable --now thermald
+sudo systemctl enable --now systemd-oomd
 
-	echo -e "$BLUE>> Install ananicy-cpp$NOCOLOR"
-	$AUR_HELPER -S ananicy-cpp cachyos-ananicy-rules-git --noconfirm --needed
-	echo -e "$PURPLE>> Enable ananicy-cpp service$NOCOLOR"
-	sudo systemctl enable --now ananicy-cpp.service
+# Optimize Swappiness
+echo "Setting swappiness to 10"
+echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.d/99-swappiness.conf
+sudo sysctl --system
 
-	echo -e "$RED>> Install earlyoom$NOCOLOR"
-	sudo pacman -S earlyoom --noconfirm --needed
-	echo -e "$YELLOW>> Enable earlyoom service$NOCOLOR"
-	sudo systemctl enable --now earlyoom
+# Disable unnecessary services
+sudo systemctl mask systemd-random-seed
+sudo systemctl mask lvm2-monitor
 
-	echo -e "$CYAN>> Disable some service$NOCOLOR"
-	sudo systemctl mask systemd-random-seed
-	sudo systemctl mask lvm2-monitor
+# Disable journald
+sudo sed -E -i 's/#Storage=auto/Storage=none/' /etc/systemd/journald.conf
 
-	echo -e "$PURPLE>> Disable journald$NOCOLOR"
-	sudo sed -E -i 's/#Storage=auto/Storage=none/' /etc/systemd/journald.conf
-
-	echo -e "$WHITE>> Setup auto cleanup cache$NOCOLOR"
-	sudo pacman -S pacman-contrib --noconfirm --needed
-	[[ -n $(pacman -Qdt) ]] && sudo pacman -Rns "$(pacman -Qdtq)" --noconfirm || echo "No orphaned packages to remove!"
-	paccache --remove --keep 1
-	paccache --remove --uninstalled --keep 0
-	sudo systemctl enable --now paccache.timer
-}
-
-main() {
-	clear
-	cat <<-EOF
-		  [*] What is your AUR helper
-
-		  [1] yay
-		  [2] paru
-
-	EOF
-
-	read -rp "[?] Select Option : "
-
-	if [[ $REPLY == "1" ]]; then
-		AUR_HELPER="yay"
-		optimize
-	elif [[ $REPLY == "2" ]]; then
-		AUR_HELPER="paru"
-		optimize
-	else
-		echo -e "\n[!] Invalid Option, Exiting...\n"
-		exit 1
-	fi
-}
-
-main
+# Auto cleanup cache
+sudo pacman -S pacman-contrib --noconfirm --needed
+[[ -n $(pacman -Qdt) ]] && sudo pacman -Rns "$(pacman -Qdtq)" --noconfirm || echo "No orphaned packages to remove!"
+paccache --remove --keep 1
+paccache --remove --uninstalled --keep 0
+sudo systemctl enable --now paccache.timer
